a799c8800802ba663fa3189ad1830f71c5abea0a
Robin Bate Boerop <robin@engineml.com>
2019-11-10 11:03:22 -0800

    Add jq to nix-prefetch-docker

--- pkgs/build-support/docker/nix-prefetch-docker.nix
+++ pkgs/build-support/docker/nix-prefetch-docker.nix
@@ -1,10 +1,10 @@
-{ stdenv, makeWrapper, nix, skopeo }:
+{ stdenv, makeWrapper, nix, skopeo, jq }:
 
 with stdenv.lib;
 
 stdenv.mkDerivation {
   name = "nix-prefetch-docker";
-
+  buildInputs = [ nix skopeo jq ];
   nativeBuildInputs = [ makeWrapper ];
 
   dontUnpack = true;
@@ -12,7 +12,7 @@ stdenv.mkDerivation {
   installPhase = ''
     install -vD ${./nix-prefetch-docker} $out/bin/$name;
     wrapProgram $out/bin/$name \
-      --prefix PATH : ${makeBinPath [ nix skopeo ]} \
+      --prefix PATH : ${makeBinPath [ nix skopeo jq ]} \
       --set HOME /homeless-shelter
   '';
 
